00dae2ada84c2b34a0e46ceca57825e98f6de731
ANGADMADHEPURA <ANGADRAJ9534@GMAIL.COM>
2022-12-13 12:01:57 +0530

    pi

--- index.js
+++ index.js
@@ -6,10 +6,15 @@ console.log(a);
 
 var a = 30;
 
-if(a <= 40){
-    var b = 80;
+if(a <= 20){
+  var b = 80;
+    
 }else{
     var c = 100;
 }
 
 console.log(a,b,c);
+
+const pi = 3.14;
+var pi = 4;
+console.log(pi);
