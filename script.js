201b6f5c0f2af916fc67a1914e0366cce1cccacf
ANGADMADHEPURA <ANGADRAJ9534@GMAIL.COM>
2022-12-14 11:46:56 +0530

    var navMenuAnchorTags = document.querySelectorAll('.nav-menu a'); console.log(navMenuAnchorTags); for(var i = 0; i < navMenuAnchorTags.length; i++){     navMenuAnchorTags[i].addEventListener('click', function(event){         event.preventDefault();         var targetSectionID = this.textContent.trim().toLowerCase();         console.log(targetSectionID);     }); }

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var navMenuAnchorTags = document.querySelectorAll('.nav-Menu a');
+var navMenuAnchorTags = document.querySelectorAll('.nav-menu a');
 console.log(navMenuAnchorTags);
 for(var i = 0; i < navMenuAnchorTags.length; i++){
     navMenuAnchorTags[i].addEventListener('click', function(event){
