85fe775ce06232a0789f8fb3faca2d710e56941d
ANGADMADHEPURA <ANGADRAJ9534@GMAIL.COM>
2022-12-14 12:01:20 +0530

    target id

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ for(var i = 0; i < navMenuAnchorTags.length; i++){
     navMenuAnchorTags[i].addEventListener('click', function(event){
         event.preventDefault();
         var targetSectionID = this.textContent.trim().toLowerCase();
+        var targetSection = document.getElementById(targetSectionID);
         console.log(targetSectionID);
     });
 }
