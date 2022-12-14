0e5d80844fff93ae6c127e98859668ac8cfe5f86
ANGADMADHEPURA <ANGADRAJ9534@GMAIL.COM>
2022-12-14 17:48:46 +0530

    auto fill skill bar

--- script.js
+++ script.js
@@ -18,7 +18,56 @@ for(var i = 0; i < navMenuAnchorTags.length; i++){
 }
 
 // Skills Bars
-$('.skills-percent').each(function(){
-    $(this).animate({
-      width:$(this).attr('data-percent')},"fast");
-    });
+// Skills Bars
+var progressBars = document.querySelectorAll(".skill-progress > div");
+var skillsContainer = document.getElementById('skills-container');
+var animationDone = false;
+
+
+
+function initialiseBars() {
+    for (var bar of progressBars) {
+        bar.style.width = 0 + '%';
+    }
+}
+
+initialiseBars();
+
+
+
+function fillBars() {
+
+    for (let bar of progressBars) {
+        let currentWidth = 0;
+        let interval = setInterval(function () {
+            let targetWidth = bar.getAttribute('data-bar-width');
+            if (currentWidth >= targetWidth) {
+                clearInterval(interval);
+                return;
+            }
+            currentWidth++;
+            bar.style.width = currentWidth + '%';
+        }, 5);
+    }
+}
+
+
+
+function checkScroll() {
+
+    var coordinates = skillsContainer.getBoundingClientRect();
+    if (!animationDone && coordinates.top <= window.innerHeight) {
+        animationDone = true;
+        fillBars();
+    } else if (coordinates.top > window.innerHeight) {
+        animationDone = false;
+        initialiseBars();
+    }
+}
+
+
+
+window.addEventListener("scroll", checkScroll);
+
+// This event fills the progress bars if they are displayed on the screen when the page is loaded.
+//window.addEventListener("load", checkScroll);
