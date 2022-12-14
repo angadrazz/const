612eacc20ae02627921a71ba27d2ba8b34c3c783
ANGADMADHEPURA <ANGADRAJ9534@GMAIL.COM>
2022-12-14 12:59:53 +0530

    decument

---
+++ resumedocument.js
@@ -0,0 +1,6 @@
+
+
+//Handle scroll event on window 
+//Check that skill selections cotainer is visible or not 
+//Ensure that initial width of colored skill divs is zero -> initialised/Reset to 0 width value 
+//Start ani ation on every skill -> increase skill width from 0 to skill level 
